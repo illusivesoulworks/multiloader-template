b07c45aeb1be569a9988b4faac3e80b7efdaf1f7
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2023-11-15 16:15:41 -0800

    Additions and tweaks

--- forge/src/main/java/com/example/examplemod/mixin/MixinTitleScreen.java
+++ forge/src/main/java/com/example/examplemod/mixin/MixinTitleScreen.java
@@ -11,10 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class MixinTitleScreen {
 
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-
-        Constants.LOG.info("This line is printed by an example mod mixin from Forge!");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-    }
+  @Inject(at = @At("HEAD"), method = "init()V")
+  private void init(CallbackInfo info) {
+    Constants.LOG.info("This line is printed by an example mod mixin from Forge!");
+    Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+  }
 }
